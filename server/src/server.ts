b4918fce1f8e4c02ce42cb41ecd6aecd0299fad6
James <james.mikesell@gmail.com>
2020-04-14 01:21:04 -0400

    refactor(): format only

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,34 +6,34 @@ import cookieParser from 'cookie-parser';
 
 export class DominoServer extends Server {
 
-    private readonly SERVER_STARTED = 'Example server started on port: ';
+  private readonly SERVER_STARTED = 'Example server started on port: ';
 
-    constructor() {
-        super(true);
-        this.app.use(bodyParser.json());
-        this.app.use(cookieParser());
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.setupControllers();
-    }
+  constructor() {
+    super(true);
+    this.app.use(bodyParser.json());
+    this.app.use(cookieParser());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.setupControllers();
+  }
 
-    private setupControllers(): void {
-        const ctlrInstances = [];
-        for (const name in controllers) {
-            if (controllers.hasOwnProperty(name)) {
-                const controller = (controllers as any)[name];
-                ctlrInstances.push(new controller());
-            }
-        }
-        super.addControllers(ctlrInstances);
+  private setupControllers(): void {
+    const ctlrInstances = [];
+    for (const name in controllers) {
+      if (controllers.hasOwnProperty(name)) {
+        const controller = (controllers as any)[name];
+        ctlrInstances.push(new controller());
+      }
     }
+    super.addControllers(ctlrInstances);
+  }
 
-    public start(port: number): void {
-        this.app.get('*', (req, res) => {
-            res.send(this.SERVER_STARTED + port);
-        });
-        this.app.listen(port, () => {
-            Logger.Imp(this.SERVER_STARTED + port);
-        });
-    }
+  public start(port: number): void {
+    this.app.get('*', (req, res) => {
+      res.send(this.SERVER_STARTED + port);
+    });
+    this.app.listen(port, () => {
+      Logger.Imp(this.SERVER_STARTED + port);
+    });
+  }
 }
 
